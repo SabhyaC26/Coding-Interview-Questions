7e2c6cc3a85fb1568d81a89e5e135c757c559f6c
ssc255 <ssc255@cornell.edu>
2019-07-29 02:05:02 -0400

    fixes

--- CrackingTheCodingInterview/Arrays and Strings/PalindromePermutation.py
+++ CrackingTheCodingInterview/Arrays and Strings/PalindromePermutation.py
@@ -1,35 +1,16 @@
 """
-Given a string, write a function to check if it is a permutation of a palin­drome. A palindrome is a word or phrase that 
-is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited 
+Given a string, write a function to check if it is a permutation of a palin­drome. A palindrome is a word or phrase that
+is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited
 to just dictionary words.
+
+I really like this problem because of the simple, elegant solution that it uses
 """
 
-#helper method to check if a given string is a palindrome
-#turns out this function is not necessary for solving the problem
-#i'll still leave it in there
-def isPal(s):
-    stack = []
-    half = int(len(s)/2)
-    #if even -> put 1/2 elts in the list
-    if len(s) % 2 == 0:     
-        for x in range(half):
-            stack.append(s[x])
-        for x in range(half):
-            if stack.pop() != s[x + half]:
-                return False
-    #if odd -> put all elts before the center elt
-    else:
-        for x in range(half):
-            stack.append(s[x])
-        for x in range(half):
-            if stack.pop() != s[x + half + 1]:
-                return False
-    return True
-            
+# this is the main (and only) function needed to solve the problem
 
-#this is the main (and only) function needed to solve the problem
+
 def palPerm(s):
-    #basically --> we want to check if there exists a permutation of (s) that is a palindrome
+    # basically --> we want to check if there exists a permutation of (s) that is a palindrome
     chars = []
     for c in s:
         if c not in chars:
@@ -41,14 +22,39 @@ def palPerm(s):
     else:
         return len(chars) == 1
 
+# helper method to check if a given string is a palindrome
+# turns out this function is not necessary for solving the problem
+# i'll still leave it in there
+
+
+def isPal(s):
+    stack = []
+    half = int(len(s)/2)
+    # if even -> put 1/2 elts in the list
+    if len(s) % 2 == 0:
+        for x in range(half):
+            stack.append(s[x])
+        for x in range(half):
+            if stack.pop() != s[x + half]:
+                return False
+    # if odd -> put all elts before the center elt
+    else:
+        for x in range(half):
+            stack.append(s[x])
+        for x in range(half):
+            if stack.pop() != s[x + half + 1]:
+                return False
+    return True
+
 
 def main():
-    print(palPerm("hello")) #should be false
-    print(palPerm("RACECAR")) #should be true
-    print(palPerm("CARERACE")) #should be true
+    print(palPerm("hello"))  # should be false
+    print(palPerm("RACECAR"))  # should be true
+    print(palPerm("CARERACE"))  # should be true
 
-    #print(isPal("ABA"))
-    #print(isPal("MALAYALAM"))
+    # print(isPal("ABA"))
+    # print(isPal("MALAYALAM"))
+
 
 if __name__ == "__main__":
     main()
