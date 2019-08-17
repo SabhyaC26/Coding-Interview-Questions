d847f745615ddf674e212f69f1224023925e0905
ssc255 <ssc255@cornell.edu>
2019-08-17 12:53:16 -0700

    matrix multiplication

--- DynamicProgramming/chainMatrixMultiplication.py
+++ DynamicProgramming/chainMatrixMultiplication.py
@@ -1,14 +1,34 @@
 """
-Given a chain of matrices (assume that they are compatible in terms of
-multiplication), in what order should you multiply the matrices,
-such that the cost of multiplying is minimized.
-
-Return only the min cost of multiplying, no need to trace back.
-
-eg. A B C D can be computed as (AB)(BC), (((AB)C)D), ... many ways to
-do this
+The Chain Matrix Multiplication Problem asks,
+given a sequence of matrices, what is the fewest
+number of operations needed to compute the product
+of all the matrices? In other words, if we were to
+parenthesize the given chain of matrix multiplications
+optimally, how many operations would it take to evaluate
+that expression?
 """
 
 
-def costOfMultiplying(A, B):
-    return A[0] * B[1] * A[1]
+def matrixChainMult(matrices):
+    def cols(i): return matrices[i][1]
+    def rows(i): return matrices[i][0]
+
+    n = len(matrices)
+    f = {}  # it can be cleaner to use a map instead of 2D array
+    for l in range(1, n + 1):
+        for i in range(0, n - l + 1):
+            if l == 1:  # base case
+                f[(i, i)] = 0
+                continue
+            k = i + l - 1
+            f[(i, k)] = min(
+                f[(i, j)] +
+                f[(j + 1, k)] +
+                (rows(i) * cols(j) * cols(k))
+                for j in range(i, k)
+            )
+    return f[(0, n - 1)]
+
+
+if __name__ == "__main__":
+    print(matrixChainMult([(2, 10), (10, 3), (3, 8)]))
