e8eb8d6891942080cd45a36305b7e2a7de26229b
ssc255 <ssc255@cornell.edu>
2019-08-23 08:02:41 -0700

    start 3sum

---
+++ Blind75/3sum.py
@@ -0,0 +1,16 @@
+"""
+Given an array nums of n integers, are there elements a, b, c in nums such
+that a + b + c = 0? Find all unique triplets in the array which gives the
+sum of zero.
+LeetCode Rating: Medium
+Link: https://leetcode.com/problems/3sum/
+"""
+
+
+def threeSum():
+    pass
+
+
+if __name__ == "__main__":
+    A = [-1, 0, 1, 2, -1, -4]
+    print(threeSum(A))
