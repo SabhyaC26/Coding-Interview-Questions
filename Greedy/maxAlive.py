284ef20b0dda5bd744554213bfe66180d7727aa7
ssc255 <ssc255@cornell.edu>
2019-08-22 17:00:33 -0700

    more questions

---
+++ Greedy/maxAlive.py
@@ -0,0 +1,4 @@
+"""
+Given a list of people with their birth years and death years, find the year
+with the highest population.
+"""
---
+++ Others/sortedItr.py
@@ -0,0 +1,17 @@
+"""
+You have three unsorted list of numbers. Design/Write function that will return
+next minimum element out of these lists (remove that element from list).
+
+Input: list1 = [5, 1, 2, 4], list2 = [4, 6, 3], list3 = [9, 0, 7]
+Output:
+next(); // 0
+next(); // 1
+next(); // 2
+next(); // 3
+next(); // 4
+next(); // 4
+next(); // 5
+next(); // 6
+next(); // 7
+next(); // 7
+"""
