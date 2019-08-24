6bc57cc8a86326a93b6477d95dda0a46e3d1411a
ssc255 <ssc255@cornell.edu>
2019-08-24 12:10:34 -0700

    more questions

---
+++ DynamicProgramming/piProblem.py
@@ -0,0 +1,9 @@
+"""
+Input -> long string of the numbers in pi and list of fav numbers
+Output -> min number of spaces that need to break up number to be in list
+of fav numbers
+"""
+
+
+def splitPi(pi):
+    pass
