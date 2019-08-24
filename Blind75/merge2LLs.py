b5f36c5207d607fd29c747c4a5d2e22ca9eddffc
ssc255 <ssc255@cornell.edu>
2019-08-24 02:51:21 -0700

    merge 2 lls

--- Blind75/merge2LLs.py
+++ Blind75/merge2LLs.py
@@ -1,6 +1,10 @@
 """
 Merge two sorted linked lists and return it as a new list. The new list
 should be made by splicing together the nodes of the first two lists.
+
+LeetCode Rating: Easy
+
+Link: https://leetcode.com/problems/merge-two-sorted-lists/
 """
 
 
