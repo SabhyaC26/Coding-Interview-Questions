afae2176aa5e94b09b97d52fd92d2f49ee588a85
ssc255 <ssc255@cornell.edu>
2019-08-26 15:50:39 -0700

    some change

---
+++ Others/meetingRooms2.py
@@ -0,0 +1,9 @@
+"""
+Given an array of meeting time intervals consisting of start and end times
+[[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms
+required.
+
+LeetCode Rating: Medium
+
+Link: https://leetcode.com/problems/meeting-rooms-ii/
+"""
