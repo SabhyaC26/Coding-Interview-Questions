7f53137a8430a654b4e5bb2ee83f375a8dc546d6
ssc255 <ssc255@cornell.edu>
2019-08-31 15:24:24 -0400

    wronggggg

--- Binary Search Tree/lca.py
+++ Binary Search Tree/lca.py
@@ -8,3 +8,12 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+
+
+def lowestCommonAncestor(root, p, q):
+    if (p.val < root.val and root.val < q.val) or (q.val < root.val and root.val < p.val):
+        return root
+    if (p.val < root.val) and (q.val < root.val):
+        return lowestCommonAncestor(root.left, p, q)
+    if (p.val > root.val) and (q.val > root.val):
+        return lowestCommonAncestor(root.right, p, q)
