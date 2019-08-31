eaa5e25f23acf090479b3594debb5698918dad1f
ssc255 <ssc255@cornell.edu>
2019-08-31 14:12:39 -0400

    works

--- Others/isBST.py
+++ Others/isBST.py
@@ -8,3 +8,19 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+
+
+def isBST(root):
+    if root is None:
+        return True
+    elif root.left is None and root.right is None:
+        return True
+    elif root.left is not None and root.right is not None:
+        if root.left <= root.val and root.val <= root.right:
+            return isBST(root.left) and isBST(root.right)
+    elif root.left is None and root.right is not None:
+        if root.right >= root.val:
+            return isBST(root.right)
+    elif root.right is None and root.left is not None:
+        if root.left <= root.val:
+            return isBST(root.left)
