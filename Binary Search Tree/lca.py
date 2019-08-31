b9df9b5c7b475328712fbb79f7c088ef45b6349f
ssc255 <ssc255@cornell.edu>
2019-08-31 15:07:14 -0400

    lca

---
+++ Binary Search Tree/lca.py
@@ -0,0 +1,10 @@
+"""
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+"""
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
