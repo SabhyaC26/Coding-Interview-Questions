8a41a2c3964319edd46f8fc61396b604b52060ac
ssc255 <ssc255@cornell.edu>
2019-08-31 22:17:59 -0400

    closest val

--- Binary Search Tree/closestVal.py
+++ Binary Search Tree/closestVal.py
@@ -1,6 +1,7 @@
 """
 Return the closes value in the BST
 """
+from queue import Queue
 
 
 class TreeNode(object):
@@ -8,3 +9,25 @@ class TreeNode(object):
         self.val = x
         self.left = None
         self.right = None
+
+
+def closestVal(root, target):
+    smallestDif = abs(root.val - target)
+    closest = root.val
+    q = Queue()
+    q.put(root)
+    while not q.empty():
+        node = q.get()
+        if node.left is not None:
+            q.put(node.left)
+            dif = abs(target-node.left.val)
+            if dif < smallestDif:
+                smallestDif = dif
+                closest = node.left.val
+        if node.right is not None:
+            q.put(node.right)
+            dif = abs(target-node.right.val)
+            if dif < smallestDif:
+                smallestDif = dif
+                closest = node.right.val
+    return closest
