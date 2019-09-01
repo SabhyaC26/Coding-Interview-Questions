231e722c02dd672c3fba38090996d8195806d954
ssc255 <ssc255@cornell.edu>
2019-08-31 22:21:51 -0400

    bst itr

---
+++ Binary Search Tree/bstIterator.py
@@ -0,0 +1,25 @@
+"""
+BST Iterator
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
