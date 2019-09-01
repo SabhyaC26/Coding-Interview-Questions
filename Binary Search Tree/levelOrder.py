0266c36e02e38463530418f1092d3c3e3a2207b2
ssc255 <ssc255@cornell.edu>
2019-08-31 21:46:57 -0400

    lev order complete

--- Binary Search Tree/levelOrder.py
+++ Binary Search Tree/levelOrder.py
@@ -14,13 +14,34 @@ class TreeNode(object):
 
 # for level order you have to use BFS
 def levelOrder(root):
+    if root is None:
+        return []
+
     q = Queue()
     q.put(root)
+    dmap = {}
+    dmap[root] = 1
+    maxd = 1
 
-    while q:
+    while not q.empty():
         node = q.get()
-        print(node.val)
         if node.left is not None:
             q.put(node.left)
+            dmap[node.left] = 0
+            dmap[node.left] = dmap[node] + 1
+            if dmap[node.left] > maxd:
+                maxd += 1
         if node.right is not None:
             q.put(node.right)
+            dmap[node.right] = 0
+            dmap[node.right] = dmap[node] + 1
+            if dmap[node.right] > maxd:
+                maxd += 1
+    bigList = []
+    for d in range(1, maxd+1):
+        dlist = []
+        for key, value in dmap.items():
+            if value == d:
+                dlist.append(key.val)
+        bigList.append(dlist)
+    return bigList
