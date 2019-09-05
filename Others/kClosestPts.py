de3b5c583f30fd213da5791155d203b67877652f
ssc255 <ssc255@cornell.edu>
2019-09-04 23:31:24 -0400

    fix heap q

--- Others/kClosestPts.py
+++ Others/kClosestPts.py
@@ -24,14 +24,11 @@ def getKClosestPtsSimple(points, k):
 
 def getKClosestPtsEfficient(points, k):
     # O(k + (n-k)logk) solution
-    # using a max heap (algo doesnt reflect bc of use of library functions)
-    ptsMap = {}
     distances = []
     for pt in points:
-        dist = getDist(pt, [0, 0])
-        distances.append(dist)
-        ptsMap[dist] = pt
-    return [ptsMap[d] for d in heapq.nsmallest(k, distances)]
+        dist = math.sqrt(pt[0]**2 + pt[1]**2)
+        distances.append((dist, pt))
+    return [d[1] for d in heapq.nsmallest(K, distances)]
 
 
 if __name__ == "__main__":
