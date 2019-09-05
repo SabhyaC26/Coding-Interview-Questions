be0417ae8a908165adf02a992c3cd69821712eb0
ssc255 <ssc255@cornell.edu>
2019-09-04 22:59:41 -0400

    no index

--- Others/topKWords.py
+++ Others/topKWords.py
@@ -4,3 +4,22 @@ Your answer should be sorted by frequency from highest to lowest.
 If two words have the same frequency, then the word with the lower
 alphabetical order comes first.
 """
+import heapq
+
+
+def topKFrequent(words, k):
+    wordCount = {}
+    for w in words:
+        if w not in wordCount.keys():
+            wordCount[w] = 0
+        wordCount[w] += 1
+    h = []
+    for key, value in wordCount.items():
+        if len(h) < k:
+            heapq.heappush(h, (value, key))
+        else:
+            top = heap[0]
+            if top[0] < value:
+                heapq.heappop(h)
+                heapq.heappush(h, (value, key))
+    return [x[1] for x in h]
