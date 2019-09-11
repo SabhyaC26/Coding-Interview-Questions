381acde4651abc0f92787277d06da57c02e42fb9
ssc255 <ssc255@cornell.edu>
2019-09-10 20:38:34 -0400

    hello

---
+++ Others/validPalindrome1.py
@@ -0,0 +1,20 @@
+def isPalindrome(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    s = s.lower()
+    s = s.replace(" ", "")
+    new_s = ''
+    for c in s:
+        if c.isalnum():
+            new_s += c
+    print(new_s)
+    p1 = 0
+    p2 = len(new_s)-1
+    while p1 < p2:
+        if new_s[p1] != new_s[p2]:
+            return False
+        p1 += 1
+        p2 -= 1
+    return True
