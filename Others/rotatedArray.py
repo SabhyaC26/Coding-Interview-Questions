6bcc610f7cc97002a712dbbe9717c60000dd74dd
ssc255 <ssc255@cornell.edu>
2019-09-13 10:18:22 -0400

    rotated arr

--- Others/rotatedArray.py
+++ Others/rotatedArray.py
@@ -1,23 +1,24 @@
-class Solution:
-    def helper(self, arr, l, h, key):
-        if l > h:
-            return -1
-        mid = (l + h) // 2
-        if arr[mid] == key:
-            return mid
-        # If arr[l...mid] is sorted
-        if arr[l] <= arr[mid]:
-            # As this subarray is sorted, we can quickly
-            # check if key lies in half or other half
-            if key >= arr[l] and key <= arr[mid]:
-                return self.helper(arr, l, mid-1, key)
-            return self.helper(arr, mid+1, h, key)
-        # If arr[l..mid] is not sorted, then arr[mid... r]
-        # must be sorted
-        if key >= arr[mid] and key <= arr[h]:
-            return self.helper(arr, mid+1, h, key)
-        return self.helper(arr, l, mid-1, key)
+# search roated array
+def searchRotated(arr, lo, hi, key):
+    if lo > hi:
+        return -1
+    mid = (lo + hi) // 2
+    if arr[mid] == key:
+        return mid
+    # arr[lo...mid] is sorted
+    if arr[lo] <= arr[mid]:
+        # now check if key is in this sorted portion or not
+        if key >= arr[lo] and key <= arr[mid]:
+            return searchRotated(arr, lo, mid-1, key)
+        else:
+            return searchRotated(arr, mid+1, hi, key)
+    # if arr[lo...mid] is not sorted, then arr[mid...r] must be sorted
+    else:
+        if key >= arr[mid] and key <= arr[hi]:
+            return searchRotated(arr, mid+1, hi, key)
+        else:
+            return searchRotated(arr, lo, mid-1, key)
 
-    def search(self, nums: List[int], target: int) -> int:
-        h = len(nums)-1
-        return self.helper(nums, 0, h, target)
+
+def caller(arr, key):
+    return searchRotated(arr, 0, len(arr)-1, key)
