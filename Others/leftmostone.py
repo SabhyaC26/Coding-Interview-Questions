0bc0d576b7252853b28251077cf6b5119a794236
ssc255 <ssc255@cornell.edu>
2019-09-16 23:35:42 -0400

    lol

---
+++ Others/leftmostone.py
@@ -0,0 +1,21 @@
+def leftmostoneindex(matrix):
+    rows = len(matrix)
+    cols = len(matrix)
+    candidate = -1
+    r = 0
+    c = cols - 1
+    while r < rows and c >= 0:
+        if matrix[r][c] == 1:
+            candidate = c
+            c -= 1
+        else:
+            r += 1
+    return candidate
+
+
+if __name__ == "__main__":
+    matrix = [[0, 0, 0, 1],
+              [0, 0, 1, 1],
+              [0, 1, 1, 1],
+              [0, 0, 0, 0]]
+    print(leftmostoneindex(matrix))
