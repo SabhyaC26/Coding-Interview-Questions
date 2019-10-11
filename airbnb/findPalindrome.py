04f5027c0c8a1f2c98f136f7b4f0df938e997d6d
ssc255 <ssc255@cornell.edu>
2019-10-11 12:34:10 -0400

    work

---
+++ airbnb/findPalindrome.py
@@ -0,0 +1,4 @@
+"""
+Given an array of strings, find if two strings concatenate to form a palindrome in linear time.
+Example: {"cat","tac", "rac","car,"ta","abc"}
+"""
---
+++ airbnb/travelBuddy.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+
+    me = {"a", "b", "c", "d"}
+    f1 = {"a", "b", "e", "f"}
+    f2 = {"a", "c", "d", "g"}
+    f3 = {"c", "f", "e", "g"}
+
+    buddies = {'friend1': f1, 'friend2': f2, 'friend3': f3}
+
+    print(buddies)
